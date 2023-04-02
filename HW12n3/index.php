10e257974836df9fc6a32f9630c4953d5f74e200
Valerii <valera_belousov@ukr.net>
2023-04-02 20:22:41 +0300

    виправлено відступи

--- HW12n3/index.php
+++ HW12n3/index.php
@@ -1,11 +1,10 @@
 <?php
-$sumNumber = function ($sumArray)
-{
-    $sum= 0;
-foreach ($sumArray as $number){
-    $sum = ($number > 10) ? ($sum += $number) : $sum ;
-}
-return  $sum;
+$sumNumber = function ($sumArray) {
+    $sum = 0;
+    foreach ($sumArray as $number) {
+        $sum = ($number > 10) ? ($sum += $number) : $sum;
+    }
+    return $sum;
 };
-$result = $sumNumber([1,5,6,10,12,16,24,30]);
+$result = $sumNumber([1, 5, 6, 10, 12, 16, 24, 30]);
 echo $result;
